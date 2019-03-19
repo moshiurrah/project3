acb0daffe9e6ae900026ec5ce18d4f4955deddc8
Mohammad Moshiur Rahman <43156585+moshiurrah@users.noreply.github.com>
2019-03-19 10:55:49 -0400

    Update ShoppingList.js

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { Container, Button, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+//import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
-import 'bootstrap/dist/css/bootstrap.css';
 
 class ShoppingList extends Component {
   static propTypes = {
